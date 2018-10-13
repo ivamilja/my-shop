487da63dd6a9ea6240a32b2568108c4e566ad982
ivana26 <42611627+ivana26@users.noreply.github.com>
2018-10-13 16:22:09 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,29 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { BootstrapNavbarComponent } from './bootstrap-navbar/bootstrap-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductViewComponent } from './product-view/product-view.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BootstrapNavbarComponent
+    BootstrapNavbarComponent,
+    HomeComponent,
+    ProductViewComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    ShoppingCartComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
