998c82a6d811e1990248910c011b4f5ec4e6e7af
ivana26 <42611627+ivana26@users.noreply.github.com>
2018-10-13 16:07:34 +0200

    Create project with firebase and bootstrap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBfDMan3ze6UX0gnF9dVOPg2b3kngNSoSs",
+    authDomain: "my-shop-17772.firebaseapp.com",
+    databaseURL: "https://my-shop-17772.firebaseio.com",
+    projectId: "my-shop-17772",
+    storageBucket: "my-shop-17772.appspot.com",
+    messagingSenderId: "627995605670"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBfDMan3ze6UX0gnF9dVOPg2b3kngNSoSs",
+    authDomain: "my-shop-17772.firebaseapp.com",
+    databaseURL: "https://my-shop-17772.firebaseio.com",
+    projectId: "my-shop-17772",
+    storageBucket: "my-shop-17772.appspot.com",
+    messagingSenderId: "627995605670"
+  }
 };
 
 /*
