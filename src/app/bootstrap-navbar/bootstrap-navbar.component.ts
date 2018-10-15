9cd140513ded4def4594a3181947f7fc18edc084
ivana26 <42611627+ivana26@users.noreply.github.com>
2018-10-15 18:28:14 +0200

    "Display current user name"

--- src/app/bootstrap-navbar/bootstrap-navbar.component.ts
+++ src/app/bootstrap-navbar/bootstrap-navbar.component.ts
@@ -1,17 +1,19 @@
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'bootstrap-navbar',
   templateUrl: './bootstrap-navbar.component.html',
   styleUrls: ['./bootstrap-navbar.component.css']
 })
 export class BootstrapNavbarComponent {
-
+  user$: Observable<firebase.User>;
   constructor(
     private afauth: AngularFireAuth
-  ) {}
-
+  ) {
+    this.user$ = afauth.authState;
+  }
   ngOnInit() {
   }
   logout() {
